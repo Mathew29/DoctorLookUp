6a0edc083f0bdddac6056c50b2ea02a67573712f
Student <example@example.com>
2019-06-07 09:33:08 -0700

    add backend and ui logic for api's

---
+++ src/doctorLookUp.js
@@ -0,0 +1,18 @@
+export class DoctorLookUp {
+  getDoctor() {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=cough&location=or-portland&skip=0&limit=10&user_key=process.env.export.apiKey`
+
+      request.onload = function() {
+        if(this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+}
