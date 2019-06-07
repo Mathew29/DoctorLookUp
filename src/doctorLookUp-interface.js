d265eccbcc8b764feb2a075f96c169a83ab42829
Student <example@example.com>
2019-06-07 09:38:07 -0700

    add html input form and add styles and bootstrap connection

--- src/doctorLookUp-interface.js
+++ src/doctorLookUp-interface.js
@@ -1,7 +1,7 @@
 import $ from 'jquery';
-// import 'bootstrap';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import './styles.css';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
 import { DoctorLookUp } from './doctorLookUp.js';
 
 $(document).ready(function() {
